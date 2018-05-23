336b2c5080429e01edc92a92075f6621aa6c3d66
daisuke kitsunai <daisuke_kitsunai@myway.co.jp>
2018-05-23 13:29:21 +0900

    アドレスを表示するようにした

--- CStudy/Source.cpp
+++ CStudy/Source.cpp
@@ -16,6 +16,9 @@ struct Test {
 /// テスト用の関数1 値渡し
 /// </summary>
 void Twice(Test t) {
+	cout << "Twice" << endl;
+	cout << "tのアドレス = " << &t << endl;
+
 	t.A *= 2;
 	t.B *= 2;
 }
@@ -24,6 +27,9 @@ void Twice(Test t) {
 /// テスト用の関数1 ポインタ渡し
 /// </summary>
 void Twice2(Test* t) {
+	cout << "Twice2" << endl;
+	cout << "tのアドレス" << t << endl;
+
 	// 等価のコード
 
 	// (*t).A *= 2;
@@ -40,15 +46,18 @@ int main(void) {
 	main_t.A = 10;
 	main_t.B = 10;
 
+	cout << "main_tのアドレス = " << &main_t << endl;
+	cout << endl;
+
 	Twice(main_t);
-	cout << "Twice" << endl;
 	cout << "main_t.A = " << main_t.A << endl;
 	cout << "main_t.B = " << main_t.B << endl;
+	cout << endl;
 
 	Twice2(&main_t);
-	cout << "Twice2" << endl;
 	cout << "main_t.A = " << main_t.A << endl;
 	cout << "main_t.B = " << main_t.B << endl;
+	cout << endl;
 
 	char w;
 	cin >> w;
